9bd52940f0a51d274ed0c07d881a624910045270
Gh0$tN0d3 <gunwo080600@naver.com>
2026-04-30 17:10:32 +0900

    Add files via upload

---
+++ 설정.py
@@ -0,0 +1,16 @@
+봇_토큰 = "MTQ5Nzg1Nzg5NjM1NDkzODg5MA.G2Sq8y.cYB_u27gK74gK-J5rajw8bIokcJxpa1aUNhP94" #봇의 토큰
+관리자아이디 = [1497519314721570848] #라이센스 뽑을 수 있는 사람의 아이디
+API_엔드포인트 = "https://discord.com/api/v9"  #웬만해선 안건드려도됨
+클라이언트_아이디 = "1497857896354938890" #디스코드 개발자 센터 Oauth2 탭에 들어가면 있는 Client ID
+클라이언트_시크릿 = "vDXiK_aAWXfbffNOqrXns_ZDw0crKpT1" #디스코드 개발자 센터 Oauth2 탭에 들어가면 있는 Client Secret
+도메인 = "https://default.internal" #복구봇을 이용할 도메인
+복구키사용웹훅 = "https://discord.com/api/webhooks/1499273125223993345/m4DwjBW94xKwrtNMs1SmT7ZNiqXqBpMFBLY7-QWiS3BRcicSHf2zNuG_xMEUZFIcguIw"
+########### 건드리지 마세요 ###########
+token = 봇_토큰
+admin_id = 관리자아이디
+api_endpoint = API_엔드포인트
+client_id = 클라이언트_아이디
+client_secret = 클라이언트_시크릿
+base_url = 도메인
+bokweb=복구키사용웹훅
+########### 건드리지 마세요 ###########
